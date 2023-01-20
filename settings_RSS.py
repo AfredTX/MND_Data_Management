8841200c259886f7abfa1c31fc0ee248a94d5eb2
AfredTX <53712537+AfredTX@users.noreply.github.com>
2023-01-20 12:46:47 -0500

    Settings Class
    The settings class contains all of the necessary contextual information required during the  intake, account creation, Google API, RSS API, data manipulation, and report generation processes.

--- settings_RSS.py
+++ settings_RSS.py
@@ -13,7 +13,7 @@ class Settings:
 
         # Set authorization settings
         #todo move credentials and alter filepath
-        with open(fr'C:\Users\fredr\PycharmProjects\MND_Data_Management(2)\Credentials\auth.txt', 'r') as file:
+        with open(<AUTH>, 'r') as file:
             auth = file.read()
         self.fred_authorization = {'Authorization': f'{auth}'}
         self.posting_headers = {
@@ -24,15 +24,15 @@ class Settings:
         self.accounts_url = 'https://apiv4.reallysimplesystems.com/accounts'
         self.activities_url = 'https://apiv4.reallysimplesystems.com/activities'
         self.documents_url = 'https://apiv4.reallysimplesystems.com/documents'
-        self.info_survey_url = 'https://forms.office.com/r/bhQvpTTyqk'
+        self.info_survey_url = <FORMS URL>
 
         # Set google sheets id's
         # todo move credentials and alter filepath
-        self.google_creds = fr"C:\Users\fredr\PycharmProjects\MND_Data_Management(2)\sheets-network-f964f31e5bf1.json"
-        self.info_session_tracker = '1yp09eMrb3wfB6HslJ0yPriuKwosNjvbZlvyQB-tAHkw'
-        self.info_session_registration = '1ooiRYTu9Jd2BgheprId8IOI1wZ88IqlGrGcav0WlLKY'
-        self.fy22_training_attendance = '1RKjwFXC4xvrSmEM3w5bwkHn4JphWHmF5nbwFMA2gH9I'
-        self.fy23_training_attendance = '1oHAwdaho-O3mhfNADSQ0Q6MzfxKcCNIYNdvBIrAwSjM'
+        self.google_creds = <GOOGLE CREDS PATH>
+        self.info_session_tracker = <URL>
+        self.info_session_registration = <URL>
+        self.fy22_training_attendance = <URL>
+        self.fy23_training_attendance = <URL>
 
         # Set url add ons
         self.record_limit = '?limit=100'
@@ -41,7 +41,7 @@ class Settings:
 
         # Set file settings
         self.directory_path = os.getcwd()
-        self.data_management_path = fr"C:\Users\fredr\OneDrive - Maryland New Directions\Data Management"
+        self.data_management_path = <ONE DRIVE DATA MANAGMENT PATH
         self.client_directory_path = fr"{self.directory_path}\client_directories"
         self.call_history = fr"{self.directory_path}\rss_data\api_call_history.json"
         self.accounts_csv = fr"{self.directory_path}\rss_data\accounts.csv"
@@ -98,7 +98,7 @@ class Settings:
         self.accepted_grants = ['CDBG', 'DOL Opioid', 'EARN', 'Grads2Careers', 'MOED Opioid','OWIF', 'OWIF-W', 'SNAP',
                                 'WIOA']
         # todo alter filepath
-        with open(fr'C:\Users\fredr\PycharmProjects\MND_Data_Management(2)\grant_cycles.json', 'r') as file:
+        with open(<GRANT CYCLES JSON PATH>, 'r') as file:
             self.grant_cycles = file.read()
 
         # Set Job Coach Settings
@@ -108,10 +108,6 @@ class Settings:
             self.rb_client_directory, self.rv_client_directory,
             self.sh_client_directory, self.ms_client_directory, self.vf_client_directory]
 
-        self.coach_contact = {'CH': 'churt@mdnewdirections.org', 'ML': 'mlewis@mdnewdirections.org',
-                              'RV': 'rvallance@mdnewdirections.org', 'RB': 'rbaker@mdnewdirections.org',
-                              'SH': 'sgibbs@mdnewdirections.org', 'MS': 'mshearn@mdnewdirections.org',
-                              'VF': 'vfrierson@mdnewdirections'}
 
         # Set Time Settings
         # noinspection PyUnresolvedReferences
@@ -128,10 +124,6 @@ class Settings:
         self.two_year = pd.to_timedelta('730 days')
 
         # pw settings
-        # todo move credentials and alter filepath
-        with open(fr'C:\Users\fredr\PycharmProjects\MND_Data_Management(2)\Credentials\pw.txt', 'r') as file:
-            pw = file.read()
-        self.passcode = pw
 
         # Set KPI columns
         self.kpi_columns = ['Account Id', 'Name', 'Grant Fund', 'Case Manager','Original Intake Date',
@@ -167,23 +159,29 @@ class Settings:
 
         # Intake document settings
         self.income_categories = ['Extremely Low Income','Low Income', 'Moderate Income']
+        
         self.income_sources = ['Alimony', 'Salary/Wages', 'Child Support', 'SSI', 'SSDI', 'Cash Assistance/TCA',
                                'Unemployment', 'Social Security', 'SNAP/Food Stamp', 'Other Sources', 'No Income']
         self.races = ['Black/African American', 'White', 'Asian', 'Black/African American and White',
                       'American Indian/Alaskan Native and Black/African American', 'Other Multi-Racial Category',
                       'American Indian/Alaskan Native and White', 'Asian and White', 'American Indian/Alaskan Native',
                       'Native Hawaiian/Other Pacific Islander']
+        
         self.barriers = ['Age', 'Appearance', 'Childcare', 'Criminal Background', 'Communication Skills', 'Disability',
                          'Education', 'Employment History', 'Family Circumstances', 'Health', 'Homeless', 'Illiteracy',
                          'Lack Job Search/Application Skills', 'Lack of Consistent Phone Number', 'Lack of Network',
                          'Resume', 'Self Esteem', 'Transportation']
+        
         self.covid_effects = ['Business Closed', 'Laid Off', 'Credit Score Damage', 'Decrease In Work Hours',
                               'Food Insecurity', 'Health Physical or Mental', 'Inability To Pay Rent', 'Lost Child Care',
                               'Left Labor Force', 'Own Business Closed', 'Youth Adverse Experience',
                               'Covid Other Check']
+        
         self.incumbent_details = ['incumbent_start_date','incumbent_employer', 'incumbent_position', 'hourly_wage',
                                   'hours_per_week']
+        
         self.incumbent_benefits = ['Medical', 'Dental', 'Retirement', 'Stock Options', 'Profit Sharing', 'Wellness']
+        
         self.rem_list = ['Contact 1 Phone', 'Contact 2 Phone', 'parole meeting', 'Last Job', 'Reason For Leaving',
                          'in custody', 'dependants in house', 'benefits received', 'total hh size', 'disabled',
                          'sixty-two plus', 'ft student', 'chargedate', 'crime', 'marylandid', 'children <18',
@@ -195,6 +193,7 @@ class Settings:
                          'not female hh', 'First Name', 'MI', 'Last Name', 'othersources', 'paroleprobation',
                          'not_latino', 'eligibletowork', 'Suffix', 'client_age', 'mentalabusenote', 'owner',
                          'snapincome', 'returning_client']
+        
         self.v14_rem_list = ['Contact 1 Phone', 'parole meeting',
                          'in custody', 'total hh size', 'disabled',
                          'sixty-two plus', 'ft student', 'marylandid', 'children <18',
@@ -210,6 +209,7 @@ class Settings:
                             'LI7','LI8','MI1','MI2','MI3','MI4','MI5','MI6','MI7','MI8','01','02','03','04','05','06',
                             '07','08','cdbghhincomename','cdbghhincomeaddress','cdbghhincomezip','cdbghhincomedate',
                             'cdbghhincomestaffname','cdbghhincomestaffdate','cdbghhincomestafftitle']
+        
         self.cdbg_race_fields = ['cdbgracename','cdbgracedate','cdbgracestaffname','cdbgracestafftitle',
                                  'cdbgracestaffdate','latino','not_latino','Black/African American','White','Asian',
                                  'Black/African American and White',
@@ -219,10 +219,6 @@ class Settings:
                                  'American Indian/Alaskan Native','Native Hawaiian/Other Pacific Islander','femalehh',
                                  'not female hh','total hh size','disabled','sixty-two plus','ft student',
                                  'children <18']
-
-
-
-
 
 
         self.required_categories = 38
@@ -240,7 +236,7 @@ class Settings:
 
         self.file_extensions = ['.pdf', '.docx', '.doc' ,'.jpeg', '.jpg', '.tif', '.JPG','.JPEG','PDF']
 
-        with open(fr'C:\Users\fredr\PycharmProjects\MND_Data_Management(2)\train_up_activity_index.json', 'r') as file:
+        with open(<TU ACTIVITY INDEX>, 'r') as file:
             self.train_up_json = file.read()
 
         self.bc_zips = [21215, 21206, 21224, 21218, 21207, 21229, 21217, 21225, 21212, 21230, 21213, 21239, 21216,
@@ -252,7 +248,7 @@ class Settings:
 
 
         # One Drive backup
-        with open(fr'C:\Users\fredr\PycharmProjects\MND_Data_Management(2)\username.txt', 'r') as file:
+        with open(<USERNAME>, 'r') as file:
             self.username = file.read()
         if self.username == "Fred":
             self.data_management_path = fr"C:\Users\fredr\OneDrive - Maryland New Directions\Data Management"
